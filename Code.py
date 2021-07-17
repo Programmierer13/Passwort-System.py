5a80163e406071d311c459418220b6e1f3276c7e
Programmierer13 <84669839+Programmierer13@users.noreply.github.com>
2021-07-17 21:06:24 +0200

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,7 @@
+print("Passwort System")
+print("Bitte gib das Passwort ein")
+Passwort = input()
+if Passwort == "Dein Passwort "
+   print("Eingeloggt ")
+else: 
+     print ("Falsches Passwort ")
